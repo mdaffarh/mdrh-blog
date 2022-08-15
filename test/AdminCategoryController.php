d40c916317a12913a9f4ff50e0f16f652e35eac5
mdaffarh <daffarizmawan@gmail.com>
2022-08-15 17:48:17 +0700

    adwpdijaw

---
+++ test/AdminCategoryController.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Cviebrock\EloquentSluggable\Services\SlugService;
+
+class AdminCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {  
+        return view('dashboard.categories.index', [
+            'categories' => Category::all()
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('dashboard.categories.create', [
+            'categories' => Category::all()
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|unique:categories|max:255',
+            'slug' => 'required|unique:categories',
+            'image' => 'image|file|max:1024'
+        ]);
+
+        if($request->file('image')){
+            $validatedData['image'] = $request->file('image')->store('category-image');
+        }
+
+        Category::create($validatedData);
+
+        return redirect('/dashboard/categories')->with('success','New category has been added!');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(category $category)
+    {
+        return view('dashboard.categories.edit', [
+            'category' => $category
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, category $category)
+    {
+        $rules = [
+            'name' => 'required|max:255',
+            'slug' => 'required',
+            'image' => 'image|file|max:1024'
+        ];
+
+        if($request->slug != $category->slug){
+            $rules['slug'] = 'required|unique:categories';
+        }
+
+        $validatedData = $request->validate($rules);
+
+        if($request->file('image')){
+            if($request->oldImage){
+                Storage::delete($request->oldImage);
+            }
+            $validatedData['image'] = $request->file('image')->store('category-image');
+        }
+
+        Category::where('id',$category->id)
+                ->update($validatedData);
+
+        return redirect('/dashboard/categories')->with('success','Category has been updated!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(category $category)
+    {
+        if($category->oldImage){
+            Storage::delete($category->oldImage);
+        }
+        Category::destroy($category->id);
+        return redirect('/dashboard/categories')->with('deleted','Category has been deleted');
+    }
+
+    public function checkSlug(Request $request)
+    {
+        $slug = SlugService::createSlug(Category::class, 'slug', $request->category);
+        return response()->json(['slug' => $slug]);
+    }
+}
